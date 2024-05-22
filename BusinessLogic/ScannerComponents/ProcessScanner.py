2fce4cf637bb0c2e08d3373a8d415a538d0dc18a
Longji Kang <kanglongjidev@gmail.com>
2024-05-22 09:12:31 +0200

    feat: add original process monitoring component

---
+++ BusinessLogic/ScannerComponents/ProcessScanner.py
@@ -0,0 +1,13 @@
+class ProcessScanner():
+    def __init__(self):
+        self.malicious_processes = [
+            'AppMarket.exe',
+            'PcyybAssistant.exe'
+        ]
+
+    def checkProcess(self, proc) -> bool:
+        if proc.name() in self.malicious_processes:
+            proc.terminate()
+            return True
+        else:
+            return False
